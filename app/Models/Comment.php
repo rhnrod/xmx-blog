bfe67d054b7911773fbbcb22da85023bbb27619a
Rhuan Emanuel <rhnrod@gmail.com>
2025-12-01 07:58:15 -0300

    feat(comment): create comment model

---
+++ app/Models/Comment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'body',
+        'likes',
+        'post_id',
+        'user_id'
+    ];
+
+    // Relacionamentos
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
