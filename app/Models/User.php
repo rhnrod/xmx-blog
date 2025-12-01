bc6675a2d18a9f8feff16c0b1544d5a9bbbd70bd
Rhuan Emanuel <rhnrod@gmail.com>
2025-12-01 07:33:37 -0300

    fix(user): adjust user model for dummyjson

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,26 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    public $incrementing = false;
+    protected $keyType = 'integer';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
-        'name',
+        'id',
+        'firstName',
+        'lastName',
         'email',
-        'password',
+        'phone',
+        'image',
+        'birthDate',
+        'address',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
+    protected $casts = [
+        'address' => 'array',
+        'birthDate' => 'date',
     ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
